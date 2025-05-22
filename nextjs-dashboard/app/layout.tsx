9e82fb910f06517a7dd80ad6d02e1337624a1372
Alitares27 <aldair2795@hotmail.com>
2025-05-22 20:36:36 -0300

    Add SideNav import and enable experimental PPR in layout and config

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,12 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import SideNav from '@/app/ui/dashboard/sidenav';
 
 export const metadata: Metadata = {
   title: 'Acme Dashboard',
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
+
+export const experimental_ppr = true;
 
 export default function RootLayout({
   children,
